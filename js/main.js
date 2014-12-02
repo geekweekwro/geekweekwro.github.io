33acc786d77bcececacfdda49111ee076264b3fe
Gosia Borzęcka <gosia.borzecka@gmail.com>
2014-12-02 11:59:45 +0100

    Added PLSSUG Group

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
     geekWeekApp.controller('GeekWeekCtrl', function ($scope) {
         $scope.groups = [
             {
-                'name': 'Women in Technology',
+                'name': 'Women in Technology, WiT',
                 'description': 'Women In Technology to społeczność Kobiet, które łączy jedna wspólna cecha – technologia. Celem grupy jest budowanie opinii, że Kobieta może być pasjonatką i wybitną specjalistką w swojej dziedzinie. Women In Technology to niezależna polska grupa, która powstała w 2009 roku, zrzesza Kobiety o zainteresowaniach technicznych i informatycznych, pomaga im osiągać swoje cele, organizuje warsztaty i szkolenia zwiększające kreatywność. Women In Technology liczy już ponad 130 aktywnych uczestników oraz dziesiątki wolnych słuchaczy, także mężczyzn. Każdy z nas słyszał o sytuacjach, w których Kobieta dostała mniejsze szanse zawodowe ze względu na kwestie pozamerytoryczne. Women In Technology przełamuje stereotypy, jest także niezależną grupą cenioną przede wszystkim za wiedzę.',
                 'logo': 'content/witLOGO.png',
                 'url': 'http://www.womenintechnology.pl',
@@ -18,11 +18,18 @@
                 'age': 2
             },
             {
-                'name': 'Wrocław Symfony Group',
+                'name': 'Wrocław Symfony Group, WSG',
                 'description': 'Wrocław Symfony Group to cykliczne spotkania dla osób zainteresowanych frameworkiem Symfony. Praktycy dla praktyków oraz osób chcących zacząć swoją przygodę z Symfony. ',
                 'logo': 'content/wsgLogo.png',
                 'url': 'http://wsg.wroclaw.pl',
                 'age': 3
+            },
+            {
+                'name': 'Polish SQL Server User Group, PLSSUG',
+                'description': 'Polish SQL Server User Group (PLSSUG) jest grupą pasjonatów systemu Microsoft SQL Server. Głównym celem jest umożliwienie jak najszerszemu gronu ludzi poznawania jednego z lepszych systemów zarządzania bazami danych oraz dzielenia się z innymi wiedzą i doświadczeniem. PLSSUG jest oficjalnym chapterem organizacji PASS (Professional Association for SQL Server) w Polsce. Nie reprezentuje firmy Microsoft, a jedynie z nią współpracuje.',
+                'logo': 'content/plssugLogo.png',
+                'url': 'http://plssug.org.pl/',
+                'age': 4
             }
     ];
 
