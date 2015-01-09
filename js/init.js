b955734fd30bff8e340ce4e2c67efce96c910788
Mirosław Pragłowski <m@praglowski.com>
2015-01-09 22:25:39 +0100

    fixed links to sponsors & parters, prepared switch to horizontal timeline when image will be available

--- js/init.js
+++ js/init.js
@@ -66,10 +66,22 @@
 ------------------------------------------------------ */
 
    $('header').css({ 'height': $(window).height() });
-   $(window).on('resize', function() {
+   function resize() {
 
         $('header').css({ 'height': $(window).height() });
         $('body').css({ 'width': $(window).width() })
+
+        // switch timeline image
+        var contentwidth = $(window).width();
+        if ((contentwidth) < '960'){
+          $('#timeline').attr('src','images/timeline.png');
+        } else {
+          $('#timeline').attr('src','images/timeline.png');
+        }
+   }
+   resize();
+   $(window).on('resize', function(){
+     resize();
    });
 
 
@@ -97,27 +109,6 @@
 
 	});
 
-
-/*----------------------------------------------------*/
-/*	Modal Popup
-------------------------------------------------------*/
-
-    $('.item-wrap a').magnificPopup({
-
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
-
-    });
-
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
-    });
-
-
 /*----------------------------------------------------*/
 /*	Flexslider
 /*----------------------------------------------------*/
@@ -132,50 +123,6 @@
       animationSpeed: 600,
       randomize: false,
    });
-
-/*----------------------------------------------------*/
-/*	contact form
-------------------------------------------------------*/
-
-   $('form#contactForm button.submit').click(function() {
-
-      $('#image-loader').fadeIn();
-
-      var contactName = $('#contactForm #contactName').val();
-      var contactEmail = $('#contactForm #contactEmail').val();
-      var contactSubject = $('#contactForm #contactSubject').val();
-      var contactMessage = $('#contactForm #contactMessage').val();
-
-      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
-      $.ajax({
-
-	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
-
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
-
-	      }
-
-      });
-      return false;
-   });
-
 
 });
 
